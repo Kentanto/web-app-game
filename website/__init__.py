c80a2ac5083a0922960d4c0e1125b693a1dba088
Kentanto <131159966+Kentanto@users.noreply.github.com>
2024-05-06 17:42:15 +0200

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-DB_NAME = "/home/kentanto65/web-app-game/instance/deathgamecharacters.db"
-
-def init_game():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "ye atomic's pretty good"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
-
-    from .views import head
-    app.register_blueprint(head, url_prefix='/')
-
-    return app
-=======
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -32,4 +14,3 @@ def init_game():
     game.register_blueprint(head, url_prefix='/')
         
     return game
->>>>>>> 04435538c6d4ea0b67cd0cb0ac646b380f3dcf40
