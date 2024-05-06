c66f4572a46131d465eedb3ba0d14ee7f40bd52d
Kentanto <holb04@icloud.com>
2024-05-06 16:07:31 +0000

    why is consoles so difficult

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -16,21 +14,3 @@ def init_game():
     game.register_blueprint(head, url_prefix='/')
 
     return game
-=======
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-DB_NAME = "/home/kentanto65/web-app-game/instance/deathgamecharacters.db"
-
-def init_game():
-    game = Flask(__name__)
-    game.config["SECRET_KEY"] = "ye atomic's pretty good"
-    game.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(game)
-    
-    from .views import head
-    game.register_blueprint(head, url_prefix='/')
-        
-    return game
->>>>>>> c80a2ac5083a0922960d4c0e1125b693a1dba088
